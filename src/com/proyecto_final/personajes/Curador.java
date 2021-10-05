54cbe068315c09e7a7aef90fea92a126ac0e9b80
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-05 15:27:19 -0600

    Create Curador class

---
+++ src/com/proyecto_final/personajes/Curador.java
@@ -0,0 +1,12 @@
+package com.proyecto_final.personajes;
+
+public class Curador extends  Personaje{
+
+    public Curador() {
+        super("Curador",300,40,10,25);
+    }
+
+    public void curar() {
+
+    }
+}
