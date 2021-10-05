bbaeff7817a0429d41f831d8c2395179908e4aa4
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-05 15:26:03 -0600

    Create Asesino class

---
+++ src/com/proyecto_final/personajes/Asesino.java
@@ -0,0 +1,12 @@
+package com.proyecto_final.personajes;
+
+public class Asesino extends Personaje{
+
+    public Asesino() {
+        super("Asesino",250,55,3,15);
+    }
+
+    public void ejecutar() {
+
+    }
+}
