0efca06d02d5f1621a8b4333df89da1adf9dcf42
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-05 15:24:12 -0600

    Create Arquero class

---
+++ src/com/proyecto_final/personajes/Arquero.java
@@ -0,0 +1,14 @@
+package com.proyecto_final.personajes;
+
+public class Arquero extends Personaje{
+
+    public Arquero() {
+        super("Arquero",150,60,5,20);
+    }
+
+    public void ignorarResistencia() {
+    }
+
+    public void pasivaEvasion(){
+    }
+}
