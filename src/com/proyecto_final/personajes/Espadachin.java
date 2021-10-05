846caf6b62a79116d083f9bb471e7f3d0338653d
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-05 15:28:16 -0600

    Create Espadachin class

---
+++ src/com/proyecto_final/personajes/Espadachin.java
@@ -0,0 +1,12 @@
+package com.proyecto_final.personajes;
+
+public class Espadachin extends Personaje{
+
+    public Espadachin() {
+        super("Espadachín",300,40,5,20);
+    }
+
+    public void toqueVenenoso() {
+
+    }
+}
