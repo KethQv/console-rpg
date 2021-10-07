8faca2248af13e6e03202243b819f91ae33ddb7d
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-07 16:12:36 -0600

    Update Juego class

--- src/com/proyecto_final/Juego.java
+++ src/com/proyecto_final/Juego.java
@@ -1,9 +1,14 @@
 package com.proyecto_final;
 
 public class Juego {
+    Jugador jugador1= new Jugador(null,null,null);
+    Jugador jugador2= new Jugador(null,null,null);
 
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
-	// write your code here
+    }
+
+    public void selecP(){
+
     }
 }
