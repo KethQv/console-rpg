bfd07333fe8cc866b7aaa24380acc929733d65b9
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-08 17:58:40 -0600

    Add sinPersonajes method and getters and setters

--- src/com/proyecto_final/Jugador.java
+++ src/com/proyecto_final/Jugador.java
@@ -1,46 +1,66 @@
 package com.proyecto_final;
 import com.proyecto_final.personajes.Personaje;
 
+import java.util.ArrayList;
+
 public class Jugador {
-    protected Personaje[] personajes = {null, null, null};
+    protected ArrayList<Personaje> personajes = new ArrayList<>();
     protected Personaje personajeActual;
 
-    public Jugador(Personaje personaje1, Personaje personaje2, Personaje personaje3) {
-        personajes[0] = personaje1;
-        personajes[1] = personaje2;
-        personajes[2] = personaje3;
-    }
-
+    public Jugador() {}
 
     public boolean haPerdido() {
-        return personajes[0].getVidaActual() != 0 || personajes[1].getVidaActual() != 0 || personajes[0].getVidaActual() != 0;
+        for (Personaje personaje : personajes) {
+            if (personaje.estaVivo()) return false;
+        }
+        return true;
     }
 
-    private void cambiarDePersonaje(int opcionCambio) {
-        switch (opcionCambio) {
-            case 1 -> {
-                if (personajes[0].getVidaActual() != 0) {
-                    personajeActual = personajes[0];
-                }
-            }
-            case 2 -> {
-                if (personajes[1].getVidaActual() != 0) {
-                    personajeActual = personajes[1];
-                }
-            }
-            case 3 -> {
-                if (personajes[2].getVidaActual() != 0) {
-                    personajeActual = personajes[2];
-                }
-            }
-        }
+//    private void cambiarDePersonaje(int opcionCambio) {
+//        switch (opcionCambio) {
+//            case 1 -> {
+//                if (personajes[0].getVidaActual() != 0) {
+//                    personajeActual = personajes[0];
+//                }
+//            }
+//            case 2 -> {
+//                if (personajes[1].getVidaActual() != 0) {
+//                    personajeActual = personajes[1];
+//                }
+//            }
+//            case 3 -> {
+//                if (personajes[2].getVidaActual() != 0) {
+//                    personajeActual = personajes[2];
+//                }
+//            }
+//        }
+//    }
+//
+//    public  void actualizarPersonaje() {
+//        for (int i = 0; i<3; i++) {
+//            if (personajeActual.getNombre().equals(personajes[i].getNombre())) {
+//                personajes[i] = personajeActual;
+//            }
+//        }
+//    }
+
+    public  boolean sinPersonajes() {
+        return personajes.size() < 3;
     }
 
-    public  void actualizarPersonaje() {
-        for (int i = 0; i<3; i++) {
-            if (personajeActual.getNombre().equals(personajes[i].getNombre())) {
-                personajes[i] = personajeActual;
-            }
-        }
+    public ArrayList<Personaje> getPersonajes() {
+        return personajes;
+    }
+
+    public void setPersonajes(ArrayList<Personaje> personajes) {
+        this.personajes = personajes;
+    }
+
+    public Personaje getPersonajeActual() {
+        return personajeActual;
+    }
+
+    public void setPersonajeActual(Personaje personajeActual) {
+        this.personajeActual = personajeActual;
     }
 }
