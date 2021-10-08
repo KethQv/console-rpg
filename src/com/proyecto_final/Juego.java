0ec57816926cb04b13e4408f2f9cacb57bbe6fa9
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-08 17:59:48 -0600

    Add inicializarPersonajes and update sleccionarPersonajes

--- src/com/proyecto_final/Juego.java
+++ src/com/proyecto_final/Juego.java
@@ -1,15 +1,20 @@
 package com.proyecto_final;
 
+import com.proyecto_final.personajes.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Juego {
-    private final static Scanner input = new Scanner(System.in);
+    private static final Scanner input = new Scanner(System.in);
 
-    private static Jugador jugador1;
-    private static Jugador jugador2;
+    private static final Jugador jugador1 = new Jugador();
+    private static final Jugador jugador2 = new Jugador();
+
+    private static final ArrayList<Personaje> personajes = new ArrayList<>();
 
     public static void main(String[] args) throws InterruptedException {
-        Pantalla.mostrarTitulo();
+//        Pantalla.mostrarTitulo();
 
         boolean seguir = true;
         do {
@@ -25,12 +30,34 @@ public class Juego {
     public static void comenzarJuego() {
         Pantalla.mostrarInstrucciones();
 
-        seleccionarPersonaje(jugador1);
-        seleccionarPersonaje(jugador2);
+        inicializarPersonajes();
 
+        seleccionarPersonaje(jugador1, 1);
+        seleccionarPersonaje(jugador2, 2);
     }
 
-    public static void seleccionarPersonaje(Jugador jugador) {
+    public static void inicializarPersonajes() {
+        personajes.add(new Arquero());
+        personajes.add(new Asesino());
+        personajes.add(new Curador());
+        personajes.add(new Espadachin());
+        personajes.add(new Mago());
+        personajes.add(new Piromano());
+        personajes.add(new Pistolero());
+        personajes.add(new Tanque());
+    }
 
+    public static void seleccionarPersonaje(Jugador jugador, int num) {
+        do {
+            int eleccion = Pantalla.mostrarSeleccion(num, personajes);
+
+            if (eleccion < 0 || eleccion >= personajes.size()) {
+                Pantalla.mostrarEleccionInvalida();
+            } else {
+                jugador.getPersonajes().add(personajes.get(eleccion));
+                personajes.remove(eleccion);
+            }
+
+        } while (jugador.sinPersonajes());
     }
 }
