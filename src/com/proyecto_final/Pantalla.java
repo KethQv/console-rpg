080a4058fde1ec38b2200eb3ab2b26eb7b15bce6
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-08 00:41:13 -0600

    Add mostrarMenu method

--- src/com/proyecto_final/Pantalla.java
+++ src/com/proyecto_final/Pantalla.java
@@ -1,6 +1,10 @@
 package com.proyecto_final;
 
+import java.util.Scanner;
+
 public class Pantalla {
+
+    private final static Scanner input = new Scanner(System.in);
 
     public static void mostrarTitulo() throws InterruptedException {
         String[] title = {
@@ -45,5 +49,22 @@ public class Pantalla {
             Thread.sleep(800);
             if (i < title.length - 1) Util.limpiarConsola();
         }
+    }
+
+    public static int mostrarMenu() {
+        String str = "" +
+                "╔═══════════════════════════════════════════════════════════════════════════════╗\n" +
+                "║      __  ____      _   ___  ___  _____" + "                                        ║\n" +
+                "║     /  |/  (_)__  (_) / _ \\/ _ \\/ ___/" + "                                        ║\n" +
+                "║    / /|_/ / / _ \\/ / / , _/ ___/ (_ /" + "                                         ║\n" +
+                "║   /_/  /_/_/_//_/_/ /_/|_/_/   \\___/" + "                                          ║\n" +
+                "║                                                                               ║\n" +
+                "║  [1] Jugar                                                                    ║\n" +
+                "║  [2] Creditos                                  Introduce el numero            ║\n" +
+                "║  [3] Salir                              correspondiente y presiona enter      ║\n" +
+                "║                                                                               ║\n" +
+                "╚═══════════════════════════════════════════════════════════════════════════════╝";
+        System.out.println(str);
+        return input.nextInt();
     }
 }
