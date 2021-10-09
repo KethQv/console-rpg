0c851a1fbafb976c9655c96f4960348a59b42a0d
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-08 22:54:14 -0600

    Update toString method

--- src/com/proyecto_final/personajes/Personaje.java
+++ src/com/proyecto_final/personajes/Personaje.java
@@ -2,7 +2,6 @@ package com.proyecto_final.personajes;
 
 import com.proyecto_final.Util;
 
-// TODO - Agregar javadoc
 public abstract class Personaje {
     protected String nombre;
     protected float vidaMaxima;
@@ -44,21 +43,28 @@ public abstract class Personaje {
 
     @Override
     public String toString() {
-        int length = "╔══════════════════════════════════════════════════╗".length();
+        int length = "╔═══════════════════════════════════════════════════════════════════════════════╗".length();
         String next = "║\n";
-        String nombre = "║ ■ " + this.nombre;
-        String vidaMaxima = "║ Vida maxima: " + this.vidaMaxima;
-        String resistencia = "║ Resistencia: " + this.resistencia;
-        String poderDeAtaque = "║ Poder de ataque: " + this.poderAtaque;
-        String puntosDeHabilidad = "║ Puntos de habilidad: " + puntosHabilidad;
+        String nombre = "║    ■ " + this.nombre;
+        String vidaMaxima = "║    Vida maxima: " + this.vidaMaxima;
+        String resistencia = "║    Resistencia: " + this.resistencia;
+        String poderDeAtaque = "║    Poder de ataque: " + this.poderAtaque;
+        String puntosDeHabilidad = "║    Puntos de habilidad: " + puntosHabilidad;
         return "" +
-                "╔═══════════════════════════════════════════════════╗\n" +
+                """
+                ╔═══════════════════════════════════════════════════════════════════════════════╗
+                ║                                                                               ║\n""" +
                 Util.strAutoLenght(length, nombre, next) +
+                """
+                ║                                                                               ║
+                ║                                                                               ║\n""" +
                 Util.strAutoLenght(length, vidaMaxima, next) +
                 Util.strAutoLenght(length, resistencia, next) +
                 Util.strAutoLenght(length, poderDeAtaque, next) +
                 Util.strAutoLenght(length, puntosDeHabilidad, next) +
-                "╚═══════════════════════════════════════════════════╝";
+                """
+                ║                                                                               ║
+                ╚═══════════════════════════════════════════════════════════════════════════════╝""";
     }
 
     public boolean estaVivo() {
