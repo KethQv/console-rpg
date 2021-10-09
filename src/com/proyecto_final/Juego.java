59fb4de84961efbd8857e6c600aaa4ac2af6d449
Kenneth Quintero <kennethgqv@gmail.com>
2021-10-09 02:13:17 -0600

    Add winning condition

--- src/com/proyecto_final/Juego.java
+++ src/com/proyecto_final/Juego.java
@@ -8,13 +8,10 @@ import java.util.Scanner;
 public class Juego {
     private static final Scanner input = new Scanner(System.in);
 
-    private static final Jugador jugador1 = new Jugador("JUGADOR 1");
-    private static final Jugador jugador2 = new Jugador("JUGADOR 2");
-
     private static final ArrayList<Personaje> personajes = new ArrayList<>();
 
     public static void main(String[] args) throws InterruptedException {
-//        Pantalla.mostrarTitulo();
+        Pantalla.mostrarTitulo();
 
         boolean seguir = true;
         do {
@@ -29,6 +26,9 @@ public class Juego {
     }
 
     public static void comenzarJuego() {
+        Jugador jugador1 = new Jugador("JUGADOR 1");
+        Jugador jugador2 = new Jugador("JUGADOR 2");
+
         Pantalla.mostrarInstrucciones();
 
         inicializarPersonajes();
@@ -39,13 +39,13 @@ public class Juego {
         while (true) {
             jugarTurno(jugador2, jugador1);
             if (jugador1.haPerdido()) {
-//                Pantalla.mostrarGanador("Jugador 2");
+                Pantalla.mostrarGanador("Jugador 2");
                 return;
             }
 
             jugarTurno(jugador1, jugador2);
             if (jugador2.haPerdido()) {
-//                Pantalla.mostrarGanador("Jugador 1");
+                Pantalla.mostrarGanador("Jugador 1");
                 return;
             }
         }
@@ -95,24 +95,44 @@ public class Juego {
                     seguir = true;
                 }
             }
+
+            if (!jugadorEnemigo.haPerdido() && !jugadorEnemigo.getPersonajeActual().estaVivo()){
+
+                Personaje personajeMuerto = jugadorEnemigo.getPersonajeActual();
+                Personaje personajeNuevo = jugadorEnemigo.getPersonajesVivos().get(0);
+
+                jugadorEnemigo.setPersonajeActual(personajeNuevo);
+
+                Pantalla.mostrarPersonajeMuerto(jugadorEnemigo, personajeMuerto, personajeNuevo);
+            }
+
         } while (seguir);
     }
 
     public static void seHaAtacado(Personaje personaje, Personaje objetivo) {
-        personaje.atacar(objetivo);
+        Pantalla.mostrarAtaque(personaje, objetivo);
     }
 
     public static void usoDeHabilidad(Personaje personaje, Personaje objetivo) {
-        personaje.habilidad(objetivo);
+        Pantalla.mostrarAtaque(personaje, objetivo);
     }
 
     public static void cambioDePersonaje(Jugador jugador) {
-        int eleccion = Pantalla.mostrarCambioPersonajes(jugador);
-        input.nextLine();
+        boolean seguir = true;
+        do {
+            int eleccion = Pantalla.mostrarPersonajesVivos(jugador);
+            if (eleccion < 0 || eleccion > jugador.getPersonajesVivos().size()) {
+                Pantalla.mostrarEleccionInvalida();
+            } else {
+                jugador.setPersonajeActual(jugador.getPersonajesVivos().get(eleccion));
+                Pantalla.mostrarCambioDePersonaje(jugador);
+                seguir = false;
+            }
+        } while(seguir);
     }
 
     public static void muestroDeEstadisticas(Personaje personaje) {
-        System.out.println(personaje.toString());
+        System.out.println(personaje.mostrarEstadisticas());
         input.nextLine();
     }
 }
