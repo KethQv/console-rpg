efbc797910884fbf476119c79cc868759b7b3236
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-08 21:48:50 -0600

    Update Arquero Class

--- src/com/proyecto_final/personajes/Arquero.java
+++ src/com/proyecto_final/personajes/Arquero.java
@@ -1,14 +1,19 @@
 package com.proyecto_final.personajes;
 
+import com.proyecto_final.Jugador;
+
 public class Arquero extends Personaje{
 
     public Arquero() {
         super("Arquero",150,60,5,20);
     }
 
-    public void ignorarResistencia() {
-    }
 
-    public void pasivaEvasion(){
+
+    @Override
+    public float habilidad(Personaje objetivo) {
+        float danio = this.poderAtaque*1.5f;
+        objetivo.danioRecibido(danio, true);
+        return danio;
     }
 }
