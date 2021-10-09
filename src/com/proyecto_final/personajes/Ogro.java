173e21b9818a99e0f0042047a31a9c0866b08f90
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-09 02:45:42 -0600

    Rename Curador to  Ogro class

---
+++ src/com/proyecto_final/personajes/Ogro.java
@@ -0,0 +1,19 @@
+package com.proyecto_final.personajes;
+import com.proyecto_final.Jugador;
+
+public class Ogro extends  Personaje{
+
+    public Ogro() {
+        super("Ogro",300,40,10,25);
+    }
+
+    public void curar() {
+
+    }
+
+    @Override
+    public float habilidad(Personaje objetivo) {
+        float danio=(poderAtaque * 1.5f)+30f;
+        return danio;
+    }
+}
