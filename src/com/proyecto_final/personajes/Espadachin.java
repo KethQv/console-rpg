8398233399cdc02c4699ba60ca7ab9454d372025
Raul Aloisy Castro Cota <raul_aloisy@live.com.mx>
2021-10-08 21:50:07 -0600

    Update Espadachin Class

--- src/com/proyecto_final/personajes/Espadachin.java
+++ src/com/proyecto_final/personajes/Espadachin.java
@@ -1,12 +1,17 @@
 package com.proyecto_final.personajes;
 
+import com.proyecto_final.Jugador;
+
 public class Espadachin extends Personaje{
 
     public Espadachin() {
         super("Espadachín",300,40,5,20);
     }
 
-    public void toqueVenenoso() {
-
+    @Override
+    public float habilidad(Personaje objetivo) {
+        float danio = this.poderAtaque*2;
+        objetivo.danioRecibido(danio,false);
+        return danio;
     }
 }
